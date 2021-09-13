e093b11e32d1095e3cd31a2808ce1bc373548c53
vitaliy <vitaliykorifa@gmail.com>
2021-09-14 00:07:10 +0300

    add implement of functions

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,5 +1,8 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  console.log(t);
+  let t = 5;
+};
 
 module.exports = { fn };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,15 @@
 'use strict';
 
-const countTypesInArray = null;
-
+const countTypesInArray = (N) => {
+  const res = {};
+  for (const i of N) {
+    const tp = typeof i;
+    if (tp in res) {
+      res[tp] += 1;
+    } else {
+      res[tp] = 1;
+    }
+  }
+  return res;
+};
 module.exports = { countTypesInArray };
